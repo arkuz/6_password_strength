48fc35af90b9d639bc9be93f9ed7a11acd59137d
arkuz <arkuzka@mail.ru>
2018-08-26 22:52:17 +0300

    added canges

--- password_strength.py
+++ password_strength.py
@@ -11,41 +11,58 @@ def get_password_strength(password):
         'special_symbol': False,
     }
 
+    message = ''
+
+    if re.match("^.*[А-я]+.*$", password):
+        sys.exit('Password must not contain russian characters.')
+
     if len(password) >= 8:
         check_list['len_pass'] = True
     else:
-        message = 'Password has less than 8 characters'
-        
+        message += 'Password has less than 8 characters. \n'
+
     if re.match("^.*[0-9]+.*$", password):
         check_list['digit_symbol'] = True
+    else:
+        message += 'Password does not contain a digit. \n'
 
     if re.match("^.*[a-z]+.*$", password):
         check_list['lower_symbol'] = True
+    else:
+        message += 'Password does not contain a lower symbol. \n'
 
     if re.match("^.*[A-Z]+.*$", password):
         check_list['upper_symbol'] = True
+    else:
+        message += 'Password does not contain a upper symbol.  \n'
 
     if re.match("^.*[\W_]+.*$", password):
         check_list['special_symbol'] = True
+    else:
+        message += 'Password does not contain a special symbol. \n'
 
     check_cost = int(10 / len(check_list))
     password_complexity = 0
 
     for check in check_list:
-        print(check + " : " + str(check_list[check]))
         if check_list[check]:
             password_complexity += check_cost
 
     return password_complexity, message
 
 
+def print_check_result(complexity, message):
+    print('Password complexity {0} out of 10.'.format(str(complexity)))
+    if complexity < 10:
+        print('-' * 30)
+        print('Best practices for password strength: \n' + message)
+    else:
+        print('You have a strong password.')
+
+
 if __name__ == '__main__':
-
-    password = input('Ipput your password: ')
-
+    password = input('Input your password: ')
     if not password:
         sys.exit('Password is empty.')
-
     complexity, message = get_password_strength(password)
-
-    print(str(complexity) + ' ' + message)
+    print_check_result(complexity, message)
