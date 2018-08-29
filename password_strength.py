e80a857e3f144f1ddb588f6ef4f4050047586b33
arkuz <arkuzka@mail.ru>
2018-08-29 14:51:51 +0300

    fixed comments

--- password_strength.py
+++ password_strength.py
@@ -1,76 +1,93 @@
 import sys
 import re
+import getpass
 
 
 def has_correct_length(password):
-    if len(password) >= 8:
+    LENGH_PASSWORD = 8
+    if len(password) >= LENGH_PASSWORD:
         return True
     else:
-        return 'Password has less than 8 characters.'
+        return False
 
 
 def has_digit_symbol(password):
-    if re.match('^.*[0-9]+.*$', password):
+    if re.search('\d', password):
         return True
     else:
-        return 'Password does not contain a digit'
+        return False
 
 
 def has_lower_symbol(password):
-    if re.match('^.*[a-z,а-я]+.*$', password):
+    if re.search('[a-z,а-я]', password):
         return True
     else:
-        return 'Password does not contain a lower symbol.'
+        return False
 
 
 def has_upper_symbol(password):
-    if re.match('^.*[A-Z,А-Я]+.*$', password):
+    if re.search('[A-Z,А-Я]', password):
         return True
     else:
-        return 'Password does not contain a upper symbol.'
+        return False
 
 
 def has_special_symbol(password):
-    if re.match('^.*[\W_]+.*$', password):
+    if re.search('\W', password):
         return True
     else:
-        return 'Password does not contain a special symbol.'
+        return False
 
 
 def get_password_strength(password):
 
-    check_tuple = (
-        has_correct_length(password),
-        has_digit_symbol(password),
-        has_lower_symbol(password),
-        has_upper_symbol(password),
-        has_special_symbol(password)
-    )
+    check_list = {
+        (
+            has_correct_length(password),
+            'Password has less than 8 characters.',
+        ),
+        (
+            has_digit_symbol(password),
+            'Password does not contain a digit',
+        ),
+        (
+            has_lower_symbol(password),
+            'Password does not contain a lower symbol.',
+        ),
+        (
+            has_upper_symbol(password),
+            'Password does not contain a upper symbol.',
+        ),
+        (
+            has_special_symbol(password),
+            'Password does not contain a special symbol.',
+        ),
+    }
 
     password_complexity = 0
+    COST_CHECK = 2
+    for check, _ in check_list:
+        if check:
+            password_complexity += COST_CHECK
 
-    for check in check_tuple:
-        if check is True:
-            password_complexity += 2
-
-    return password_complexity, check_tuple
+    return password_complexity, check_list
 
 
-def print_check_result(complexity, check_tuple):
-    print('Password complexity {0} out of 10.'.format(str(complexity)))
+def print_check_result(complexity, check_list):
+    print('Password complexity {0} out of 10.'.format(complexity))
     if complexity == 10:
         print('You have a strong password.')
     else:
         print('-' * 30)
-        for check in check_tuple:
-            if check is not True:
-                print(check)
+        for check, message in check_list:
+            if not check:
+                print(message)
 
 
 if __name__ == '__main__':
-    password = input('Input your password: ')
+    password = getpass.getpass(prompt='Input your password: ')
     if not password:
         sys.exit('Password is empty.')
 
-    complexity, check_tuple = get_password_strength(password)
-    print_check_result(complexity, check_tuple)
+    complexity, check_list = get_password_strength(password)
+    print_check_result(complexity, check_list)
